b405edebd65c8155396c730942e473224c39ee11
mpsingh17 <mpsingh17@outlook.co.nz>
2020-05-03 17:06:42 +1200

    initial commit.

---
+++ sandbox/app.js
@@ -0,0 +1,40 @@
+// var, let and const
+// var name = "John";
+// console.log(name);
+
+// var greeting;
+// console.log(greeting);
+// greeting = "Hello";
+// console.log(greeting);
+
+let name = "John";
+console.log(name);
+
+// variable can have letters, numbers, _, and $  
+
+// Log to console.
+// console.log("Hello World!");
+// console.log(123);
+// console.log(true);
+// console.log([1,2,3]);
+// console.log({a:1, b:2});
+
+// console.table({a:1, b:2, c:3, d: "hello"});
+
+// let err = "this is an error log";
+// console.error(err);
+
+// console.warn("This is a warning");
+
+// console.clear();
+
+// console.time("heavy function");
+//     console.log("Heavy output");
+//     console.log("Heavy output");
+//     console.log("Heavy output");
+//     console.log("Heavy output");
+//     console.log("Heavy output");
+//     console.log("Heavy output");
+//     console.log("Heavy output");
+//     console.log("Heavy output");
+// console.timeEnd("heavy function");
